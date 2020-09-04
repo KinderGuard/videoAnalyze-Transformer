f50c0bb6ea3ed264733acc51729046db3a542d0a
함정민 <jm0308@naver.com>
2020-09-04 09:43:26 +0900

    origin transformer net 수정

--- TransformerNet.py
+++ TransformerNet.py
@@ -1,6 +1,6 @@
 import torch
-import torch.nn.functional as F
-from torch import nn
+import torch.nn.functional as F #nn과 같은 모듈이 모아져 있지만 함수의 input으로 반드시 연산이 되어야 하는 값을 받음
+from torch import nn #Deep learning model에 필요한 모듈이 모아져 있는 패키지
 import numpy as np
 import math
 import torchvision
@@ -19,6 +19,7 @@ class FeedForward(nn.Module):
         nn.init.normal(self.linear_1.weight, std=0.001)
         nn.init.normal(self.linear_2.weight, std=0.001)
 
+    # 모델이 학습데이터 입력받아서 forward propagation 진행시키는 함수
     def forward(self, x):
         x = self.dropout(F.relu(self.linear_1(x)))
         x = self.linear_2(x)
@@ -29,7 +30,7 @@ class FeedForward(nn.Module):
 class Norm(nn.Module):
     def __init__(self, d_model, eps=1e-6, trainable=True):
         super(Norm, self).__init__()
-        self.size = d_model
+        self.size = int(d_model)
         # create two learnable parameters to calibrate normalisation
         if trainable:
             self.alpha = nn.Parameter(torch.ones(self.size))
@@ -39,6 +40,7 @@ class Norm(nn.Module):
             self.bias = nn.Parameter(torch.zeros(self.size), requires_grad=False)
         self.eps = eps
 
+    # 모델이 학습데이터 입력받아서 forward propagation 진행시키는 함수
     def forward(self, x):
         norm = self.alpha * (x - x.mean(dim=-1, keepdim=True)) \
                / (x.std(dim=-1, keepdim=True) + self.eps) + self.bias
@@ -48,6 +50,7 @@ class Norm(nn.Module):
 # Standard positional encoding (addition/ concat both are valid)
 class PositionalEncoder(nn.Module):
     def __init__(self, d_model, max_seq_len=80):
+        #nn.Module.__init__()실향하는 코드
         super(PositionalEncoder, self).__init__()
         self.d_model = d_model
         pe = torch.zeros(max_seq_len, d_model)
@@ -57,9 +60,12 @@ class PositionalEncoder(nn.Module):
                     math.sin(pos / (10000 ** ((2 * i) / d_model)))
                 pe[pos, i + 1] = \
                     math.cos(pos / (10000 ** ((2 * (i + 1)) / d_model)))
+        #지정된 위치에 삽입되어 있는 크기 1의 차원을 가진 새 tensor반환
         pe = pe.unsqueeze(0)
+        #weight와 bias 생성
         self.register_buffer('pe', pe)
 
+    # 모델이 학습데이터 입력받아서 forward propagation 진행시키는 함수
     def forward(self, x):
         # make embeddings relatively larger
         x = x * math.sqrt(self.d_model)
@@ -69,8 +75,13 @@ class PositionalEncoder(nn.Module):
         num_feature = x.size(2)
         spatial_h = x.size(3)
         spatial_w = x.size(4)
+
+        #Variable로 forward pass계산, gradient계산 필요 없음
+        #Variable은 backpropagation을 수행하는 backward method 제공함
+        #variables는 tensor를 감싸고, gradients가 역으로 흐를 수 있도록 tensor사이에 연산 체인을 구축함
         z = Variable(self.pe[:, :seq_len], requires_grad=False)
         z = z.unsqueeze(-1).unsqueeze(-1)
+        #dimension이 더 큰 크기로 확장된 해당 텐서의 새 view를 반환
         z = z.expand(batch_size, seq_len, num_feature, spatial_h, spatial_w)
         x = x + z
         return x
@@ -89,18 +100,22 @@ def attention(q, k, v, d_k, mask=None, dropout=None):
         output = dropout(output)
     return output
 
-
+#Action Transformer Unit
 class TX(nn.Module):
     def __init__(self, d_model=64, dropout=0.3):
         super(TX, self).__init__()
         self.d_model = d_model
         # no of head has been modified to encompass : 1024 dimension
+        #nn.Dropout(p: float = 0.5, inplace: bool = False): training 중 베르누이 분포의 표본을 사용하여 확률 p로 입력 tensor의 일부 요소를 부작위로 0으로 설정함
+        #성공률이 p인 실험에서 성공이면 X = 1, 실패이면 X = 0이라 할 때, 확률변수 X의 확률분포를 모수 p인 베르누이 분포라 함.
         self.dropout = nn.Dropout(dropout)
         self.dropout_2 = nn.Dropout(dropout)
         self.norm_1 = Norm(d_model)
         self.norm_2 = Norm(d_model)
-        self.ff = FeedForward(d_model, d_ff=d_model / 2)
+        #FeedForward모듈로~
+        self.ff = FeedForward(d_model, d_ff=int(d_model / 2))
 
+    # 모델이 학습데이터 입력받아서 forward propagation 진행시키는 함수
     def forward(self, q, k, v, mask=None):
         # q: (b , dim )
         b = q.size(0)
@@ -109,27 +124,31 @@ class TX(nn.Module):
         q_temp = q.unsqueeze(1)
         q_temp = q_temp.expand(b, t, dim)
         # q,k,v : (b, t , d_model=1024 // 16 )
+        #attention 모듈로~
         A = attention(q_temp, k, v, self.d_model, mask, self.dropout)
         # A : (b , d_model=1024 // 16 )
         q_ = self.norm_1(A + q)
+        #FeedForward한 모델을 dropout하고 norm한 게 새로운 query다~
         new_query = self.norm_2(q_ + self.dropout_2(self.ff(q_)))
         return new_query
 
-
+#Tail에서 호출함
 class Block_head(nn.Module):
     def __init__(self, d_model=64, dropout=0.3):
         super(Block_head, self).__init__()
+        #TX모듈로~
         self.T1 = TX()
         self.T2 = TX()
         self.T3 = TX()
 
+    # 모델이 학습데이터 입력받아서 forward propagation 진행시키는 함수
     def forward(self, q, k, v, mask=None):
         q = self.T1(q, k, v)
         q = self.T2(q, k, v)
         q = self.T3(q, k, v)
         return q
 
-
+#q,k,v 등장
 class Tail(nn.Module):
     def __init__(self, num_classes, num_frames, head=16):
         super(Tail, self).__init__()
@@ -138,19 +157,28 @@ class Tail(nn.Module):
         self.head = head
         self.num_features = 2048
         self.num_frames = num_frames
+        #linear의 input sample size
         self.d_model = self.num_features / 2
+        self.d_model = int(self.d_model)
         self.d_k = self.d_model // self.head
+        #4D이상의 input에 Batch Normalization적용
+        #Batch Normalization: 내부 공변량(covariate)를 줄여 심층 네트워크 교육을 가속화할 수 있음
+        #공변량: 실험결과의 독립변수 이외에 종속변수에 영향을 줄 수 있는 잡음인자(실험자가 통제하고자 하는 변수)
         self.bn1 = nn.BatchNorm2d(self.num_features)
+        #Norm Layer
         self.bn2 = Norm(self.d_model, trainable=False)
 
+        #PositionalEncoder 모듈로~
         self.pos_embd = PositionalEncoder(self.num_features, self.num_frames)
         self.Qpr = nn.Conv2d(self.num_features, self.d_model, kernel_size=(7, 4), stride=1, padding=0, bias=False)
 
         self.head_layers = []
         for i in range(self.head):
+            #Block_head모듈로~
             self.head_layers.append(Block_head())
 
         self.list_layers = nn.ModuleList(self.head_layers)
+        #input데이터에 선형 변환 적용: y = xA^T + b
         self.classifier = nn.Linear(self.d_model, num_classes)
         # resnet style initialization
         nn.init.kaiming_normal(self.Qpr.weight, mode='fan_out')
@@ -160,16 +188,20 @@ class Tail(nn.Module):
         nn.init.constant(self.bn1.weight, 1)
         nn.init.constant(self.bn1.bias, 0)
 
+    #모델이 학습데이터 입력받아서 forward propagation 진행시키는 함수
     def forward(self, x, b, t):
+        #BatchNorm2d실행
         x = self.bn1(x)
         # stabilizes the learning
         x = x.view(b, t, self.num_features, self.spatial_h, self.spatial_w)
+        #PositionalEncoder 모듈로~
         x = self.pos_embd(x)
         x = x.view(-1, self.num_features, self.spatial_h, self.spatial_w)
         x = F.relu(self.Qpr(x))
         # x: (b,t,1024,1,1) since its a convolution: spatial positional encoding is not added
         # paper has a different base (resnet in this case): which 2048 x 7 x 4 vs 16 x 7 x 7
         x = x.view(-1, t, self.d_model)
+        #Norm Layer실행
         x = self.bn2(x)
         # stabilization
         q = x[:, t / 2, :]  # middle frame is the query
@@ -180,6 +212,7 @@ class Tail(nn.Module):
         k = k.view(b, t, self.head, self.d_k)
         v = v.view(b, t, self.head, self.d_k)
 
+        #dim1, dim2 교환
         k = k.transpose(1, 2)
         v = v.transpose(1, 2)
         #  q: b, 16, 64
@@ -188,11 +221,15 @@ class Tail(nn.Module):
         for i in range(self.head):
             outputs.append(self.list_layers[i](q[:, i], k[:, i], v[:, i]))
 
+        #주어진 일련의 seq tensor들을 주어진 차원으로 연결
         f = torch.cat(outputs, 1)
+
+        #특정 dim에 Lp normalization수행
         f = F.normalize(f, p=2, dim=1)
         # F.norma
         if not self.training:
             return f
+        #f에 대한 선형 변환 수행
         y = self.classifier(f)
         return y, f
 
@@ -202,14 +239,22 @@ class Tail(nn.Module):
 class Semi_Transformer(nn.Module):
     def __init__(self, num_classes, seq_len):
         super(Semi_Transformer, self).__init__()
+        #residual learning framework(이미지 분류용 CNN모델)ReLU사용하는 거~!
+        #pretrained model = True -> weight를 캐시 디렉토리에 다운로드함
         resnet50 = torchvision.models.resnet50(pretrained=True)
+        #torch.nn.Sequential(*args: Any)순차적 컨테이너. 모듈들이 들어온 순서대로 constructor에 추가됨
         self.base = nn.Sequential(*list(resnet50.children())[:-2])
         self.tail = Tail(num_classes, seq_len)
 
+    # 모델이 학습데이터 입력받아서 forward propagation 진행시키는 함수
     def forward(self, x):
-        b = x.size(0)
-        t = x.size(1)
+        b = x.size(0)#batch size
+        t = x.size(1)#..frame수??
+        #.view(): 텐서의 형상 변환(reshape)이나 차원을 늘리거나 줄일 때 사용
+        #dimension, size
         x = x.view(b * t, x.size(2), x.size(3), x.size(4))
+        #resNet어쩌구 뭔가 해놓기
         x = self.base(x)
         # x: (b,t,2048,7,4)
+        #모듈 Tail로~
         return self.tail(x, b, t)
--- main.py
+++ main.py
@@ -1,2 +1,82 @@
+import TransformerNet
 from TransformerNet import *
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+import cv2
+import imutils
 
+plt.ion()
+
+def count_frames_manual(video):
+    # initialize the total number of frames read
+    total = 0
+    # loop over the frames of the video
+    while True:
+        # grab the current frame
+        (grabbed, frame) = video.read()
+
+        # check to see if we have reached the end of the
+        # video
+        if not grabbed:
+            break
+        # increment the total number of frames read
+        total += 1
+    # return the total number of frames in the video file
+    return total
+
+
+cap = cv2.VideoCapture('./videos/test_video1.mp4')
+total = count_frames_manual(cap)
+TransformerNet.Semi_Transformer(2, total)
+
+# params for ShiTomasi corner detection
+feature_params = dict(maxCorners=100,
+                      qualityLevel=0.1,
+                      minDistance=7,
+                      blockSize=7)
+
+# Parameters for lucas kanade optical flow
+lk_params = dict(winSize=(15, 15),
+                 maxLevel=2,
+                 criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+
+# Create some random colors
+color = np.random.randint(0, 255, (100, 3))
+
+# Take first frame and find corners in it
+ret, old_frame = cap.read()
+#old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
+p0 = cv2.goodFeaturesToTrack(old_frame, mask=None, **feature_params)  # Shi-Tomasi의 코너점
+# print(p0)  # shape : (코너점 갯수, 1, 2)[[[a,b]], ..., [[c,d]]]
+
+while True:
+    ret, frame = cap.read()
+    #blank_image = None  # 빈 이미지
+    # print(frame.shape) # 높이, 너비, 채널의 수
+    if ret:
+        #frame = imutils.resize(frame, width=800)  # frame 크기 재조정
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        height, weight, channel = frame.shape
+        #blank_image = np.zeros((height, weight, 3), np.uint8)  # frame과 크기가 같은 빈 이미지 만들기
+
+        # param : 이전 프레임, 추적할 이전 포인트, 다음 프레임 등을 인자로 전달
+        # return : 이전 프레임에서 추적할 포인트가 연속된 다음 프레임에서 추적될 경우 상태값 1을 반환, 아님 0
+        #          추적할 포인트가 이동한 새로운 위치값도 함께 반환
+        # calculate optical flow
+        p1, st, err = cv2.calcOpticalFlowPyrLK(old_frame, frame_gray, p0, None, **lk_params)  # 이전 프레임, 이전 점, 다음 프레임
+        # Select good points
+        good_new = p1[st == 1]  # 다음 프레임에서 추척할 포인트가 이동한 위치값들의 배열.
+
+        for new in good_new:
+            y, f = TransformerNet.Semi_Transformer.forward(new)
+            print(y)
+            print('\n')
